c6c1cdef9a6a1982b211b372d1256c5916875976
Paul Luu <paul_luu.work@hotmail.com>
2018-09-16 14:22:51 -0400

    FE 0.1
    -Added base scenes
    -Added Scenemanager script
    -Added basic assets

---
+++ Friendly Encounters/Assets/Scripts/StateManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StateManager : MonoBehaviour {
+
+    public const int MENUSTATE = 0;
+    public const int LOGINSTATE = 1;
+    public const int REGISTERSTATE = 2;
+    public const int PLAYSTATE = 3;
+    public const int LEVELSTATE = 4;
+    public const int MINIGAMESTATE = 5;
+
+    public void PlayButton() {
+        SceneManager.LoadScene(PLAYSTATE);
+    }
+
+}
