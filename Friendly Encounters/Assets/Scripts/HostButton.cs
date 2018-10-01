d6fcfa33b20263d80ceb463393a9a789bf7a6819
Paul Luu <paul_luu.work@hotmail.com>
2018-09-30 21:48:19 -0400

    Added TutorialState and added more to network script
    -Asset made sharper to fit pixel art
    -Can host games using Unity's mutiplayer API

---
+++ Friendly Encounters/Assets/Scripts/HostButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HostButton : MonoBehaviour {
+
+    GameObject networkContainer;
+    MyNetworkManager networkManager;
+
+    void Awake(){
+        networkContainer = GameObject.FindGameObjectWithTag("NetworkManager");
+        networkManager = networkContainer.GetComponent<MyNetworkManager>();
+        GetComponent<Button>().onClick.AddListener(StartHosting);
+    }
+
+    void StartHosting() {
+        networkManager.StartHosting();
+    }
+}
