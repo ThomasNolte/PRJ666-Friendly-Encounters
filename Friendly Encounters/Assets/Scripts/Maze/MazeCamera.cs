11906d1d7cad6c7e05e134c312df25c851769629
chrispyTofu <40364581+chrispyTofu@users.noreply.github.com>
2018-11-23 10:32:26 -0500

    lighting camera changes to maze

--- Friendly Encounters/Assets/Scripts/Maze/MazeCamera.cs
+++ Friendly Encounters/Assets/Scripts/Maze/MazeCamera.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class MazeCamera : MonoBehaviour
 {
     [HideInInspector]
     public Transform playerTransform;
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
-
+    float timeLeft = 5;
+    void Awake()
+    {
+        Camera.main.orthographicSize = 12;
+        RenderSettings.ambientLight = Color.white;
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0) {
+            Camera.main.orthographicSize = 6;
+            RenderSettings.ambientLight = Color.gray;
+        }
+     
+    }
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
